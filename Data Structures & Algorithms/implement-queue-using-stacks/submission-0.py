526ec7618bb2e8342beaa7a722c0a96a35d43a64
Lex Tran <58812503+iamthaoly@users.noreply.github.com>
2026-09-07 18:06:23 +0700

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.py
@@ -0,0 +1,41 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = deque()
+        self.stack2 = deque()
+
+    def push(self, x: int) -> None:
+        # self.self.stack2.append(x)
+        # while self.stack1:
+        #     self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        # self.stack2.pop()
+        # self.stack1, self.stack2 = self.stack2, self.stack1
+        while len(self.stack1) > 1:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def peek(self) -> int:
+        while len(self.stack1) > 1:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1[0]
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def empty(self) -> bool:
+        return len(self.stack1) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
